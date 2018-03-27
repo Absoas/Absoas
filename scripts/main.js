aa1640f73d55ff2ebb588803b346278e6b9c6af3
YbCloud <abso8787@naver.com>
2018-03-27 11:53:54 +0900

    second commit

--- scripts/main.js
+++ scripts/main.js
@@ -1,13 +1,23 @@
- var myHeading = document.querySelector('h1');
- myHeading.innerHTML = 'Cloud Sevice<br>2018-1'; //innerHTML  : HTML 을 찾아가서 변경하겠다.
+var myHeading = document.querySelector('h1');
+myHeading.innerHTML = 'Cloud Service<br>2018-1';
+function getIdPw() {
+    var id = prompt('ID 입력', '');
+    alert(id + '가 로그인함.');
+    var passwordSystem = '12345';
+    var password = prompt('PW 입력', '');
+    if (password === passwordSystem) alert('정상 로그인!');
+    else alert('비밀번호 오류');
+}
 
- var text = 'This is a text';
- text = 18.5;
- var a = 33.7;
- var b = a*text;
- var iceCream = 'chocolate';
- if(iceCream === 'icecream'){
-alert('chocolate');
- }else{
-alert('iceCream');
- }
+document.querySelector('h1').onclick = function () {
+    getIdPw();
+}
+
+var myImg = document.querySelector('img');
+myImg.onclick = function () {
+    var src = myImg.getAttribute('src');
+    if (src === 'images/firefox-icon.png')
+        myImg.setAttribute('src', 'images/mokwon.png');
+    else
+        myImg.setAttribute('src', 'images/firefox-icon.png');
+}
