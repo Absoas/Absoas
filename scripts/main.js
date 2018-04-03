3eaaf1ad481ffcff7ad82876621b2526ea21864e
YbCloud <abso8787@naver.com>
2018-04-03 12:31:28 +0900

    first commit

--- scripts/main.js
+++ scripts/main.js
@@ -2,9 +2,12 @@
 var id;
 var password;
 
-function getIdPw(){
-   id = prompt('ID 입력', '');
+function SetIdPw(){
+   id = prompt('당신이 가입할 ID 입력', '');
    password = prompt(id+'가 사용할 초기 비번 입력', '');
+
+   localStorage.setItem('id',id);
+   localStorage.setItem('password',password);
 }
 
 function SetNull(){
@@ -13,19 +16,19 @@ function SetNull(){
 }
 
 SetNull();
+SetIdPw();
 
 document.querySelector('html').onclick = function() {
-  getIdPw();
 
+  var gId = prompt('가입한 ID입력', '');
+  var  gPw = prompt(gId+'로 로그인 하시겠습니까?? 로그인 하시려면 비밀번호를 입력하세요.', '의 비밀번호 입력');
 
-  if(password === '1234'){
-    localStorage.setItem('id',id);
+  if(password == gPw){
     alert('로그인 되었습니다.');
     var myHeading = document.querySelector('h1');
-    myHeading.innerHTML = id + '<br>Homepage';
+    myHeading.innerHTML = gId + '<br>Homepage';
+  
   }else{
     alert('패스워드가 틀렸습니다.');
-    getIdPw();
   }
 }
-
